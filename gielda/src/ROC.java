325ad7ffb5fb23999ae44b50a0868d30235fe7b1
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-05-18 09:37:13 +0200

    ROC update do poprawionej wersji

--- gielda/src/ROC.java
+++ gielda/src/ROC.java
@@ -4,147 +4,109 @@ import java.util.Calendar; //import kalendarza
 //(klasa pomocnicza do timera
 import java.util.TimerTask; //klasa pomocnicza do timera
 
-public class ROC { //klasa obslugi wskaznika RSI
+public class ROC { 
+
     //deklaracja zmiennych glownych
     int N = 14; //liczba sesji historycznych
-    int seconds = 60; //czas agregowania notowań (uruchamianie timera w zależności od przedziału czasu notowań, np. minutowe, godzinne)
-    int decyzja = 0; //1 - kup; -1 - sprzedaj; 0 - nic nie rob
-    double[] vecNotowan = new double[N];
-    //wektor notowan - biezace notowanie + 13 poprzednich
+    int seconds = 60; //czas agregowania notowań 
+    /*
+     * Dla ROC z N okresów potrzebujemy N + 1 wartości:
+     *
+     * vecNotowan[0] = aktualna cena
+     * vecNotowan[N] = cena sprzed N okresów
+     *
+     * Dla N = 14 potrzebujemy indeksów od 0 do 14.
+     */
+    double[] vecNotowan = new double[N + 1];
 
-    void RunRSI() { //obliczanie wskaznika RSI
-        Timer timer1 = new Timer();//utworzenie obiektu timer
-        timer1.schedule(new TimerRun(), 0, seconds*1000);
+    int decyzja = 0; //1 - kup; -1 - sprzedaj; 0 - nic nie rob
+
+    void RunROC() { //obliczanie wskaznika ROC
+        Timer timer = new Timer();//utworzenie obiektu timer
+        timer.schedule(new TimerRun(), 0, seconds*1000);
         //konfiguracja timera
     }
 
     class TimerRun extends TimerTask {
-        public void run()
-        {
-            try {//wyłapywanie wyjątków
-                //kod znajdujący się pomiędzy
-                //nawiasami klamrowymi try
-                //jest analizowany pod kątem
-                //wystąpienia wyjątków
-                ReadData RD = new ReadData();
 
-//utworzenie obiektu klasy ReadData
-//(odczytujacej biezaca watrtosc notowania)
 
-                for (int i=N-1; i>0; i--)
-                //pętla przesuwająca wartości notowań
-                //tak, aby zawsze było 14 ostatnich
-                //notowań w wektorze
-                {
-                    vecNotowan[i] = vecNotowan[i-1];
+        public void run() {
+
+            try {
+                
+                loadTestData();
+
+                Double roc = calculateROC();
+
+                if (roc == null) {
+                    decyzja = 0;
+
+                    System.out.println("--------");
+                    System.out.println("Nie można obliczyć ROC.");
+                    System.out.println("Cena sprzed " + N + " okresów wynosi 0.0");
+                    System.out.println("Decyzja: " + decyzja);
+
+                    return;
                 }
 
-                vecNotowan[0] = RD.getQuotation();
-                System.out.println(RD.getQuotation());
-                //przepisanie do pierwszego elementu
-                //wektora aktualnej wartości notowania
-                //odczytanej za pomocą metody
-                //getQuotation (klasa ReadData)
-
-                vecNotowan[1] = 10.0;
-                vecNotowan[2] =	12.5;
-                vecNotowan[3] = 13.5;
-                vecNotowan[4] = 11.0;
-                vecNotowan[5] =	10.5;
-                vecNotowan[6] = 14.0;
-                vecNotowan[7] = 14.0;
-                vecNotowan[8] = 12.5;
-                vecNotowan[9] = 11.5;
-                vecNotowan[10] = 10.5;
-                vecNotowan[11] = 12.0;
-                vecNotowan[12] = 15.0;
-                vecNotowan[13] = 15.5;
-
-                //np. 15- 13 bedzie jako wzrost notowane
-                //Sprawdzanie wartości ( od n odejmujemy n+1) - od mniejszej wiekszosc??
-
-					/*System.out.println(vecNotowan[0]);
-					System.out.println(vecNotowan[1]);
-					System.out.println(vecNotowan[2]);
-					System.out.println(vecNotowan[3]);
-					System.out.println(vecNotowan[4]);
-					System.out.println(vecNotowan[5]);
-					System.out.println(vecNotowan[6]);
-					System.out.println(vecNotowan[7]);
-					System.out.println(vecNotowan[8]);
-					System.out.println(vecNotowan[9]);
-					System.out.println("--------"); */
-
-                double wzrosty = 0.0; //srednia wartosc wzrostu z N sesji
-                double spadki = 0.0; //srednia wartosc spadku z N sesji
-                double RS = 0.0;//wartość RS ze wzoru (patrz dokumentacja)
-                double ROCValue = 0.0;//w tej zmiennej przechowywana
-                //jest wartość wskaznika RSI (patrz dokumentacja) .
-                int licz_wzrosty = 0;//licznik wzrostów w celu
-                //obliczenia średniej wzrostów
-                int licz_spadki = 0;//licznik spadków w celu
-                //obliczenia średniej spadków
-
-                for (int i=0; i<N-1; i++) {//obliczanie wzrostów
-                    if (vecNotowan[i] > vecNotowan[i+1]) {
-                        //jeśli wartość następna jest większa od poprzedniej
-                        //to jest wzrost
-
-                        wzrosty = wzrosty + (vecNotowan[i] - vecNotowan[i+1]);
-                        //do aktualnej wartości wzrostów dodajemy
-                        //różnicę pomiędzy wartością następną, a poprzednią
-                        licz_wzrosty++;//zwiększenie wartości licznika
-                        //wzrostów o 1
-
-                    }
-                    //obliczanie spadków
-                    if ((vecNotowan[i] < vecNotowan[i+1])) {
-                        //jeśli wartość następna jest mniejsza od poprzedniej
-                        //to jest spadek
-                        spadki = spadki - (vecNotowan[i] - vecNotowan[i+1]);
-                        //od aktualnej wartości spadków odejmujemy
-                        //różnicę pomiędzy wartością następną, a poprzednią
-                        licz_spadki++;//zwiększenie wartości licznika
-                        //spadków o 1
-
-                    }
-                }
-
-
-                //System.out.println(wzrosty);
-                //System.out.println(spadki);
-
-                if (licz_wzrosty > 0)//sprawdzanie, czy
-                //nie będzie dzielenia przez 0
-                {
-
-                    wzrosty = wzrosty/licz_wzrosty;
-                    //obliczamy średnią wzrostów
-
-                }
-                if (licz_spadki > 0) //sprawdzanie, czy
-                //nie będzie dzielenia przez 0
-                {
-
-                    spadki = spadki/licz_spadki;
-                    //obliczamy średnią spadków
-                }
-
-                if (spadki > 0) RS = wzrosty/spadki;//sprawdzenie,
-                //czy nie będzie dzielenia przez 0
-                ROCValue = 100 - (100/(1+RS));//obliczanie wskaznika RSI
+                decyzja = calculateDecision(roc);
 
                 System.out.println("--------");
-                System.out.println(ROCValue);
-
-                decyzja = 0;
-                if (ROCValue >= 70) decyzja = -1;//decyzja sprzedaży
-                //(patrz dokumentacja)
-                if (ROCValue <= 30) decyzja = 1;//decyzja kupna
-                //(patrz dokumentacja)
+                System.out.println("Aktualna cena: " + vecNotowan[0]);
+                System.out.println("Cena sprzed " + N + " okresów: " + vecNotowan[N]);
+                System.out.println("ROC: " + roc);
+                System.out.println("Decyzja: " + decyzja);
+ 
             }
+            catch(Exception e) {
+                e.printStackTrace(); // dla verbosity procesu - potem można wyłączyć
 
-            catch(Exception e) {} //obsluga wyjatkow
+            } 
         }
     }
+
+    private void loadTestData() {
+
+        vecNotowan[0] = 16.0;  // aktualna cena
+        vecNotowan[1] = 10.0;
+        vecNotowan[2] = 12.5;
+        vecNotowan[3] = 11.5;
+        vecNotowan[4] = 13.5;
+        vecNotowan[5] = 11.5;
+        vecNotowan[6] = 10.5;
+        vecNotowan[7] = 14.0;
+        vecNotowan[8] = 16.0;
+        vecNotowan[9] = 16.5;
+        vecNotowan[10] = 15.5;
+        vecNotowan[11] = 12.0;
+        vecNotowan[12] = 14.5;
+        vecNotowan[13] = 15.5;
+        vecNotowan[14] = 13.0; // cena sprzed 14 okresów
+    }
+
+    private Double calculateROC() {
+        
+        double cenaAktualna = vecNotowan[0];
+        double cenaSprzedNOkresow = vecNotowan[N];
+
+        if (cenaSprzedNOkresow == 0.0) {
+            return null;
+        }
+
+        return ((cenaAktualna - cenaSprzedNOkresow) / cenaSprzedNOkresow) * 100.0;
+    }
+
+    private int calculateDecision(double roc) {
+
+        if (roc > 0.0) {
+            return 1;
+        }
+
+        if (roc < 0.0) {
+            return -1;
+        }
+
+        return 0;
+    }
 }
+
